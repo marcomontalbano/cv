0d51d6d47ad2b5b130f34a28b6e522733a49b089
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-05-11 10:23:04 +0200

    Add Profile component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import { h } from 'preact';
 import ItemsWithCompany from './ItemsWithCompany';
+import Profile from './Profile';
 
 type Props = {
     url: string;
@@ -8,6 +9,7 @@ type Props = {
 
 const App = ({ data }: Props): h.JSX.Element => (
     <div className="container">
+        <Profile profile={data.profile} />
         <ItemsWithCompany items={data.experiences} title="Work Experiences" />
         <ItemsWithCompany items={data.courses} title="Courses" />
     </div>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,45 @@
+import { h, Fragment } from 'preact';
+import { format } from 'date-fns';
+
+import Section from './Section';
+
+type Props = {
+    profile: Profile;
+};
+
+type Picture = {
+    src: string;
+    alt: string;
+};
+
+const formatDate = (date: string): string => format(new Date(date), 'dd MMMM yyyy');
+
+const Picture = ({ src, alt }: Picture): h.JSX.Element => <img height="80" src={`.${src}`} alt={alt} />;
+
+const Profile = ({ profile }: Props): h.JSX.Element => (
+    <Fragment>
+        <Section sidebar={<Picture src={profile.picture} alt={profile.fullname} />}>
+            <h1>{profile.fullname}</h1>
+            <div>
+                <span className="color-palette-1 with-margin-right">Address</span> {profile.address}
+            </div>
+            <div>
+                <span className="color-palette-1 with-margin-right">Email</span> {profile.email}
+            </div>
+            <div>
+                <span className="color-palette-1 with-margin-right">Web</span> {profile.web}
+            </div>
+            <div>
+                <span className="color-palette-1 with-margin-right">Date of birth</span> {formatDate(profile.born_date)}
+            </div>
+            <div>
+                <span className="color-palette-1 with-margin-right">Nationality</span> {profile.nationality}
+            </div>
+        </Section>
+        <Section sidebarHasTitle sidebar="position">
+            <big>{profile.position}</big>
+        </Section>
+    </Fragment>
+);
+
+export default Profile;
