b4d30d15259f5689b7c04efa5bd46cbeac01efb8
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-05-12 18:56:10 +0200

    Add skills field as markdown

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -1,6 +1,7 @@
 import { h, Fragment } from 'preact';
 import Section from '../Section';
 import Languages from './Languages';
+import Markdown from '../Markdown';
 
 type Props = {
     skill: Skill;
@@ -8,10 +9,13 @@ type Props = {
 
 const Skill = ({ skill }: Props): h.JSX.Element => (
     <Fragment>
-        <Section sidebarHasTitle sidebar="Skills" />
+        <Section sidebarHasTitle sidebar="Personal Skills" />
         <Section sidebar="Native language">{skill.nativeLanguage.join(', ')}</Section>
         <Languages languages={skill.languages} />
         <Section sidebar="Driving licence">{skill.drivingLicense.join(', ')}</Section>
+        <Section sidebar="Skills">
+            <Markdown markdown={skill.skills} />
+        </Section>
     </Fragment>
 );
 
