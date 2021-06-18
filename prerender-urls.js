ad4a405ba29745bf36c0e18b2dd35bcccc2e7b33
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-06-18 20:36:19 +0200

    Update dependencies

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const { getContent } = require('netlify-cms-json-exporter');
 
-const configPath = path.resolve('src', 'static', 'admin', 'config.yml');
-const contentPath = path.resolve('content');
+module.exports = function () {
+    const configPath = path.resolve('src', 'static', 'admin', 'config.yml');
+    const contentPath = path.resolve('content');
 
-module.exports = [
-    {
-        url: '/',
-        title: 'Homepage',
-        data: getContent(configPath, contentPath),
-    },
-];
+    return [
+        {
+            url: '/',
+            title: 'home',
+            data: getContent(configPath, contentPath),
+        },
+    ];
+};
