dfea10bc23133b3292aa81963cf5e1b50b9ba256
Marco Montalbano <marcomontalbano.work@gmail.com>
2026-05-05 09:04:54 +0200

    chore: replace showdown with comark

--- src/components/Markdown.test.tsx
+++ src/components/Markdown.test.tsx
@@ -1,14 +1,13 @@
-import { render } from '@testing-library/preact'
+import { render, waitFor } from '@testing-library/preact'
 import { describe, expect, it } from 'vitest'
 import Markdown from './Markdown'
 
 describe('Markdown', () => {
-  it('should render a given markdown as html', () => {
+  it('should render a given markdown as html', async () => {
     const { getByText } = render(<Markdown markdown="# Hello World!" />)
-    expect(
-      getByText('Hello World!', {
-        selector: 'h1',
-      }),
-    ).toBeDefined()
+    const h1 = await waitFor(() =>
+      getByText('Hello World!', { selector: 'h1' })
+    )
+    expect(h1).toBeDefined()
   })
 })
--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,34 +1,24 @@
+import { render } from '@comark/html'
 import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
-import showdown from 'showdown'
 
 type Props = {
   markdown: string
 }
 
-const converter = new showdown.Converter({
-  simplifiedAutoLink: true,
-  strikethrough: true,
-  emoji: true,
-  tables: true,
-  tasklists: true,
-  simpleLineBreaks: true,
-})
-
-const convertMarkdownToHtml = (markdown: string): string =>
-  converter.makeHtml(markdown)
+const convertMarkdownToHtml = async (markdown: string): Promise<string> =>
+  render(markdown)
 
 const Markdown = ({ markdown }: Props): h.JSX.Element => {
-  const sanitizedMarkdown = markdown.replace(/\\$/gm, '')
-
-  const [html, setHtml] = useState(() =>
-    typeof window !== 'undefined'
-      ? ''
-      : convertMarkdownToHtml(sanitizedMarkdown),
-  )
+  const [html, setHtml] = useState<string>('')
 
   useEffect(() => {
-    setHtml(convertMarkdownToHtml(sanitizedMarkdown))
+    async function convert() {
+      const html = await convertMarkdownToHtml(markdown)
+      setHtml(html)
+    }
+
+    convert()
   }, [markdown])
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />
